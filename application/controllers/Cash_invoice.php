50684608b27bff5b3146100c1f51eeec2c8818a3
Joash Noble <noblejjoash@gmail.com>
2021-07-01 11:41:06 +0800

    Weight in Sales Module

--- application/controllers/Cash_invoice.php
+++ application/controllers/Cash_invoice.php
@@ -202,6 +202,7 @@ class Cash_invoice extends CORE_Controller
                 $batch_no=$this->input->post('batch_no',TRUE);
                 $cost_upon_invoice=$this->input->post('cost_upon_invoice',TRUE);
                 $is_parent=$this->input->post('is_parent',TRUE);
+                $weight=$this->input->post('weight',TRUE);
 
                 $m_products=$this->Products_model;
 
@@ -219,6 +220,7 @@ class Cash_invoice extends CORE_Controller
                     $m_invoice_items->inv_line_total_price=$this->get_numeric_value($inv_line_total_price[$i]);
                     $m_invoice_items->inv_tax_amount=$this->get_numeric_value($inv_tax_amount[$i]);
                     $m_invoice_items->inv_non_tax_amount=$this->get_numeric_value($inv_non_tax_amount[$i]);
+                    $m_invoice_items->weight = $this->get_numeric_value($weight[$i]);
 
                     $m_invoice_items->is_parent=$this->get_numeric_value($is_parent[$i]);
                     if($is_parent[$i] == '1'){
@@ -323,6 +325,7 @@ class Cash_invoice extends CORE_Controller
                 $batch_no=$this->input->post('batch_no',TRUE);
                 $cost_upon_invoice=$this->input->post('cost_upon_invoice',TRUE);
                 $is_parent=$this->input->post('is_parent',TRUE);
+                $weight=$this->input->post('weight', TRUE);
 
                 $m_products=$this->Products_model;
 
@@ -340,6 +343,7 @@ class Cash_invoice extends CORE_Controller
                     $m_invoice_items->inv_line_total_price=$this->get_numeric_value($inv_line_total_price[$i]);
                     $m_invoice_items->inv_tax_amount=$this->get_numeric_value($inv_tax_amount[$i]);
                     $m_invoice_items->inv_non_tax_amount=$this->get_numeric_value($inv_non_tax_amount[$i]);
+                    $m_invoice_items->weight = $this->get_numeric_value($weight[$i]);
 
                     $m_invoice_items->is_parent=$this->get_numeric_value($is_parent[$i]);
                     if($is_parent[$i] == '1'){
