3b775efb1651ceeb951c72afccc3837a7f9aa763
Joash Noble <noblejjoash@gmail.com>
2021-07-09 13:19:10 +0800

    Label for Product Prices - Nepo

--- application/views/products_view.php
+++ application/views/products_view.php
@@ -363,7 +363,7 @@
 
 
                                                                 <div class="form-group" style="margin-bottom:0px;">
-                                                                    <label class="">Wholesaler Price :</label>
+                                                                    <label class="">City Distributor / Wholesaler Price :</label>
                                                                     <div class="input-group">
                                                                             <span class="input-group-addon">
                                                                                 <i class="fa fa-toggle-off"></i>
@@ -387,7 +387,7 @@
 
 
                                                                 <div class="form-group" style="margin-bottom:0px;">
-                                                                    <label class="">Distributor's Price :</label>
+                                                                    <label class="">Branch Price :</label>
                                                                     <div class="input-group">
                                                                             <span class="input-group-addon">
                                                                                 <i class="fa fa-toggle-off"></i>
